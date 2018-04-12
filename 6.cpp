dc9781e78c59350a03fa50234141722856061926
rawkush <ankushrawat950@gmail.com>
2018-04-12 20:14:49 +0530

    created all fuctions for calculating volumes

--- 6.cpp
+++ 6.cpp
@@ -6,6 +6,18 @@ float calcVolume(float lenght) // for calclating volume of cube
   return lenght*lenght*lenght;   // calclating an returning the volume pf cube
 }
 
+float calcVolume(float lenght, float breadth, float height)// for volume of cuboid
+{
+  return lenght*breadth*height;
+}
+
+float calcVolume (float radius, float lenght)// for claculating volume of cylinder
+{
+
+  return 3.14*r*r*h;
+}
+
+
  int main()
   {
 
