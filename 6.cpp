bfbf712256db61fde4694933413d8d918274921f
rawkush <ankushrawat950@gmail.com>
2018-04-12 20:23:52 +0530

    variable created in main() nd cube volume function triggred

--- 6.cpp
+++ 6.cpp
@@ -26,8 +26,13 @@ float calcVolume (float radius, float lenght)// for claculating volume of cylind
 
  int main()
   {
+volume findVolume;
+float lenght;
+cout<<"enter the lenght of side of cube ";
+cin>>lenght;
+cout <<findVolume(lenght) << '\n';
 
-
+//cout <<  << '\n';
 
   return 0;
 }
