999245cf8d4ce0faecc48d94df06d610d597a3f1
rawkush <ankushrawat950@gmail.com>
2018-04-13 10:43:03 +0530

    condtructor created

--- 5.cpp
+++ 5.cpp
@@ -1,10 +1,33 @@
 #include<iostream>
 using namespace std;
 
-float
+class A
+{
+  int value;
 
- int main()
+public:
+
+  A(int val) // for initiLIAtion
   {
+    value=val;
+  }
+
+};
+
+class B
+{
+  int value;
+  public:
+B(int val) // for initialisation
+{
+  value=val;
+}
+
+
+};
+
+int main()
+{
 
 
 
