71bb6ce6e574db6cdefbf07c064ac5a4949504c5
Ankush Rawat <ankush.rawat@warehousenow.in>
2022-03-13 19:37:02 +0530

    added leetcode solutions

--- leetcode/array/2.remove_element.cpp
+++ leetcode/array/2.remove_element.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
 
-    
+    vector<int> nums{0,1,2,2,3,0,4,2};
+    int val=2;
+    int j=0;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i]==val){
+            continue;
+        }
+        nums[j++]=nums[i];
+    }
 
     return 0;
 }
