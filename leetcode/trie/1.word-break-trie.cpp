53c6f97c1d0b2eaa3dd02f62e4bcac804eef1892
rawkush <ankushrawat950@gmail.com>
2022-08-04 23:16:11 +0530

    added word break sol by trie and memorization

--- leetcode/trie/1.word-break-trie.cpp
+++ leetcode/trie/1.word-break-trie.cpp
@@ -1,64 +1,102 @@
 #include <iostream>
 #include <unordered_map>
-#include<vector>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
-class TrieNode{
+class TrieNode
+{
 public:
   char data;
-  TrieNode **children;
+  unordered_map<char, TrieNode *> children;
   bool isTerminal;
-  TrieNode(char data){
+  TrieNode(char data)
+  {
     this->data = data;
-    children = new TrieNode*[26];
     isTerminal = false;
-    for(int i = 0; i < 26;i++){
-      children[i] = NULL;
-    }
   }
 };
 
-class Trie{
+class Trie
+{
+public:
   TrieNode *root;
-  public:
-  Trie(){
+  Trie()
+  {
     root = new TrieNode('\0');
   }
 
-  void addWord(string s){
-    TrieNode *head=root;
-    for(int i = 0; i <s.size(); i++){
-      if(head->children[s[i]-'a'] == NULL){
-        head->children[s[i]-'a'] = new TrieNode(s[i]);
-      }else{
-        head=head->children[s[i]-'a'];
+  void addWord(string s)
+  {
+    TrieNode *head = root;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+      if (head->children.count(s[i]) == 0)
+      {
+        head->children[s[i]] = new TrieNode(s[i]);
       }
+      head = head->children[s[i]];
     }
-    head->isTerminal=true;
+    head->isTerminal = true;
   }
 
-  bool searchWord(string s){
-    TrieNode *head =root;
-    for(int i=0;i< s.size(); i++){
-      if(head->children[s[i]-'a']== NULL) return false;
-      head = head->children[s[i]-'a'];
+  bool searchWord(string s)
+  {
+    TrieNode *head = root;
+    for (int i = 0; i < s.size(); i++)
+    {
+      if (head->children[s[i] - 'a'] == NULL)
+        return false;
+      head = head->children[s[i] - 'a'];
     }
     return head->isTerminal;
   }
 };
 
-// bool wordBreak(string s, vector<string>& wordDict) {
-//   unordered_map<string, int> map;
-//   for(auto word: wordDict){
-//     map[word]++;
-//   }
-//   unordered_map<string, bool> mem;
-//   return wordBreakHelper(s, map, mem);
-// }
+bool wordBreakHelper(string s, Trie &trie, int i, unordered_map<int, bool> &mem)
+{
 
-int main(){
-  Trie myTrie;
-  myTrie.addWord("leet");
-  myTrie.addWord("code");
-  cout<<myTrie.searchWord("leet");
+  if (mem.find(i) != mem.end())
+    return mem[i];
+  if (i >= s.size())
+    return true;
+  int startIndex = i;
+  auto head = trie.root;
+  for (int j = i; j < s.size(); j++)
+  {
+    if (head->children.find(s[j]) == head->children.end())
+    {
+      mem[i] = false;
+      return false;
+    }
+    head = head->children[s[j]];
+    if (head->isTerminal && wordBreakHelper(s, trie, j + 1, mem))
+    {
+      mem[j] = true;
+      return true;
+    }
+  }
+  mem[i] = false;
+  return false;
+}
+
+bool wordBreak(string s, vector<string> &wordDict)
+{
+  Trie trie;
+  unordered_map<int, bool> mem;
+  for (auto word : wordDict)
+  {
+    trie.addWord(word);
+  }
+  return wordBreakHelper(s, trie, 0, mem);
+}
+
+int main()
+{
+  vector<string> wordDict;
+  wordDict.push_back("apple");
+  wordDict.push_back("pen");
+  cout << wordBreak("applepenapple", wordDict);
   return 0;
 }
