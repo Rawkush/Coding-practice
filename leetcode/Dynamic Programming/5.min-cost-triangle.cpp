6a449630b778cf33e09c8858dca80c618353435a
rawkush <ankushrawat950@gmail.com>
2022-08-22 17:12:00 +0530

    added min cost path in triangle DP solution

--- leetcode/Dynamic Programming/5.min-cost-triangle.cpp
+++ leetcode/Dynamic Programming/5.min-cost-triangle.cpp
@@ -1,6 +1,3 @@
-
-
-
 int minimumTotalHelper(vector<vector<int>> & triangle, int row, int col, vector<vector<int*>> &mem){
   if(row>=triangle.size() || col>=triangle[row].size()) return INT_MAX;
   if(row==triangle.size()-1){
@@ -16,4 +13,23 @@ int minimumTotalHelper(vector<vector<int>> & triangle, int row, int col, vector<
 int minimumTotal(vector<vector<int>>& triangle) {
   vector<vector<int*>>mem(201,vector<int*>(201, NULL));
  return minimumTotalHelper(triangle, 0,0, mem);
+}
+
+// DP
+int minimumTotal(vector<vector<int>>& triangle) {
+ vector<vector<int>>mem(201,vector<int>(201,0));
+ int row = triangle.size();
+ int col = triangle[triangle.size()-1].size();
+
+  // fill last row of memo
+ for(int i=0; i< col; i++) {
+  mem[row-1][i]=  triangle[row-1][i];
+ }
+
+ for(int i=row-2; i>=0; i--){
+  for(int j=0; j<triangle[i].size(); j++){
+    mem[i][j] = triangle[i][j] + min(mem[i+1][j], mem[i+1][j+1]);
+  }
+ }
+  return mem[0][0];
 }
