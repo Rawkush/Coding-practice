72894cd40c4398ce5279de75a6e9ada55336c455
Ankush Rawat <ankushrawat950@gmail.com>
2023-03-28 00:30:54 +0530

    Time: 14 ms (53.3%), Space: 10.4 MB (81.60%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -10,40 +10,54 @@
  */
 class Solution {
 public:
-  bool isEven(int num){
-    return num%2==0;
-  }
-  ListNode* oddEvenList(ListNode* head) {
-    if(head==NULL) return NULL;
-    ListNode *ohead = head;
-    ListNode *otail = head;
-    ListNode *ehead = NULL;
-    ListNode *etail =NULL;
-    head= head->next;
-    int count = 2;
-    while(head!=NULL){
-      ListNode *next = head->next;
-      head->next = NULL;
-      if(isEven(count)){
-        if(ehead==NULL){
-          ehead = head;
-          etail = head;
-        }else{
-          etail->next = head;
-          etail = head;
-        }
-      }else{
-        otail->next=head;
-        otail = head;
-      }
-      count++;
-      head=next;
-    }
+  // bool isEven(int num){
+  //   return num%2==0;
+  // }
+  // ListNode* oddEvenList(ListNode* head) {
+  //   if(head==NULL) return NULL;
+  //   ListNode *ohead = head;
+  //   ListNode *otail = head;
+  //   ListNode *ehead = NULL;
+  //   ListNode *etail =NULL;
+  //   head= head->next;
+  //   int count = 2;
+  //   while(head!=NULL){
+  //     ListNode *next = head->next;
+  //     head->next = NULL;
+  //     if(isEven(count)){
+  //       if(ehead==NULL){
+  //         ehead = head;
+  //         etail = head;
+  //       }else{
+  //         etail->next = head;
+  //         etail = head;
+  //       }
+  //     }else{
+  //       otail->next=head;
+  //       otail = head;
+  //     }
+  //     count++;
+  //     head=next;
+  //   }
+  //   otail->next=ehead;
+  //   return ohead;
+  // }
 
-    if(ehead==NULL) return ohead;
-    if(ohead==NULL) return ehead;
-    
-    otail->next=ehead;
-    return ohead;
+// 2nd solution
+  ListNode* oddEvenList(ListNode* head) {
+    if(head==NULL || head->next==NULL || head->next->next==NULL) return head;
+    ListNode *otail = head;
+    ListNode *ehead = head->next;
+    ListNode *etail = head->next;
+    while(otail->next && etail->next){
+      //odd->next is even 
+      // even->next is odd
+      otail->next=etail->next;
+      otail= otail->next;
+      etail->next= otail->next;
+      etail = etail->next;
+    }
+    otail->next= ehead;
+    return head;
   }
 };
