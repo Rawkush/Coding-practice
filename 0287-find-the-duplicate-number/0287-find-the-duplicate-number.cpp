ebd284de66653556de54e6c36431b1ef39fbaca2
Ankush Rawat <ankushrawat950@gmail.com>
2023-08-08 21:37:07 +0530

    Time: 71 ms (98.61%), Space: 61.3 MB (40.46%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -6,31 +6,31 @@ public:
     use array as hashmap
   */
 
-  int helper(vector<int>& nums, int idx){
-    if(idx>=nums.size()) return -1;
-    int itm = nums[idx];
-    int di = helper(nums, idx+1);
-    if(di> 0) return di;
-    if(nums[itm]==itm && itm!=idx) return itm;
-    nums[itm] = itm;
-    return -1;
-  }
+//   int helper(vector<int>& nums, int idx){
+//     if(idx>=nums.size()) return -1;
+//     int itm = nums[idx];
+//     int di = helper(nums, idx+1);
+//     if(di> 0) return di;
+//     if(nums[itm]==itm && itm!=idx) return itm;
+//     nums[itm] = itm;
+//     return -1;
+//   }
 
-  int findDuplicate(vector<int>& nums) {
-   return helper(nums, 0);
-  }
+//   int findDuplicate(vector<int>& nums) {
+//    return helper(nums, 0);
+//   }
 
 
   /**
   -ve marking
   */
-  // int findDuplicate(vector<int>& nums) {
-  //   for(int i=0; i<nums.size(); i++){
-  //     int index = abs(nums[i]);
-  //     int itemAtIdx = nums[index];
-  //     if(itemAtIdx<0) return index;
-  //     nums[index] = -nums[index];
-  //   }
-  //   return 0;
-  // }
+  int findDuplicate(vector<int>& nums) {
+    for(int i=0; i<nums.size(); i++){
+      int index = abs(nums[i]);
+      int itemAtIdx = nums[index];
+      if(itemAtIdx<0) return index;
+      nums[index] = -nums[index];
+    }
+    return 0;
+  }
 };
