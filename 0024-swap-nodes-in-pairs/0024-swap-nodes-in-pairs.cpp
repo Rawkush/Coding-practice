a2a280692448997782ce592c1253966489f7badc
Ankush Rawat <ankushrawat950@gmail.com>
2023-08-09 21:52:30 +0530

    Time: 0 ms (100%), Space: 7.5 MB (69.76%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -12,29 +12,40 @@ class Solution {
 public:
 
 
+
+  /**
+  *  iterative
+  */
+  // ListNode* swapPairs(ListNode* head) {
+  //   if(head==NULL || head->next ==NULL ) return head;
+  //   ListNode* prev = NULL;
+  //   ListNode* curr =head;
+  //   while(curr!=NULL && curr->next!=NULL){
+  //     ListNode* next = curr->next;
+  //     if(prev==NULL){
+  //       head= next;
+  //     }else{
+  //       prev->next= next;
+  //     }
+  //     curr->next = next->next;
+  //     next->next = curr;
+  //     prev= curr;
+  //     curr= curr->next;
+  //   }
+  //   return head;
+  // }
+
+// Recusive
   ListNode* swapPairs(ListNode* head) {
     if(head==NULL || head->next ==NULL ) return head;
-
-    ListNode* prev = NULL;
-    ListNode* curr =head;
-    ListNode* newHead= NULL;
-    while(curr!=NULL && curr->next!=NULL){
-      cout<< curr->val<< " ";
-
-      ListNode* next = curr->next;
-      if(prev==NULL){
-        head= next;
-        newHead= next;
-      }else{
-        prev->next= next;
-      }
-      curr->next = next->next;
-      next->next = curr;
-      prev= curr;
-      curr= curr->next;
-    }
-    return head;
+    ListNode* rhead = swapPairs(head->next->next);
+    ListNode* curr = head;
+    ListNode* next= curr->next;
+    curr->next = rhead;
+    next->next = curr;
+    return next;
   }
+
 };
 
 
