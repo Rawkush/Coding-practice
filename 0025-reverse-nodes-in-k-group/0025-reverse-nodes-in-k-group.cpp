74cecd583dd6855e0527ecc4d002196ab85e018c
Ankush Rawat <ankushrawat950@gmail.com>
2023-08-09 23:49:26 +0530

    Time: 18 ms (43.2%), Space: 11.6 MB (7.35%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -32,21 +32,41 @@ public:
     }
   }
 
+
+  // ListNode* helper(ListNode* prev, ListNode*head, ListNode* tail){
+
+  // }
+
   ListNode* reverseKGroup(ListNode* head, int k) {
     if(head==NULL || head->next==NULL) return head;
-    ListNode* tail= head;
-    ListNode* newHead = head;
+    ListNode* newHead = NULL;
+    ListNode* curr = head;
+    ListNode* tail =head;
+    ListNode* prev = NULL;
     int i = 1;
     while(tail!=NULL && tail->next!=NULL && i<k){
+      // prev=tail;
       tail=tail->next;
       i++;
+
+      if(i==k){
+        cout<<" s "<< curr->val<<" t "<<tail->val<<" \n";
+        ListNode* next = tail->next;
+        tail->next =NULL;
+        pair<ListNode*, ListNode*> rl =  reverseList(curr);
+        if(newHead==NULL){
+          newHead = rl.first;
+        }else{
+          prev->next = rl.first;
+        }
+        curr=next;
+        prev = rl.second;
+        rl.second->next = next;
+        tail= next;
+        i=1;
+      }
     }
-    if(i<k) return head;
-    ListNode* rt = reverseKGroup(tail->next,k); //right part head
-    tail->next =NULL;
-    pair<ListNode*, ListNode*> reversed = reverseList(head);
-    newHead = reversed.first;
-    reversed.second->next = rt;
-    return newHead;
+    if(newHead) return newHead;
+    return head;
   }
 };
