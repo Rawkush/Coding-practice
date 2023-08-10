d0c05539cf13e4683ecd6a541b1df3d092b10af8
Ankush Rawat <ankushrawat950@gmail.com>
2023-08-10 12:53:46 +0530

    Time: 11 ms (91.51%), Space: 11.4 MB (41.7%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,7 +11,9 @@
 class Solution {
 public:
 
-  // ListNode
+/**
+* iterative
+*/
 
   pair<ListNode*, ListNode*> reverseList(ListNode* head) {
     ListNode* prev=NULL;
@@ -25,17 +27,6 @@ public:
     return make_pair(prev, newtail );
   }
 
-  void print(ListNode*head, ListNode*tail){
-    while(head!=tail && head!=NULL){
-      // cout<<head->val<<" ";
-      head=head->next;
-    }
-  }
-
-
-  // ListNode* helper(ListNode* prev, ListNode*head, ListNode* tail){
-
-  // }
 
   ListNode* reverseKGroup(ListNode* head, int k) {
     if(head==NULL || head->next==NULL) return head;
@@ -45,12 +36,10 @@ public:
     ListNode* prev = NULL;
     int i = 1;
     while(tail!=NULL && tail->next!=NULL && i<k){
-      // prev=tail;
       tail=tail->next;
       i++;
 
       if(i==k){
-        cout<<" s "<< curr->val<<" t "<<tail->val<<" \n";
         ListNode* next = tail->next;
         tail->next =NULL;
         pair<ListNode*, ListNode*> rl =  reverseList(curr);
@@ -59,14 +48,94 @@ public:
         }else{
           prev->next = rl.first;
         }
-        curr=next;
+       
         prev = rl.second;
         rl.second->next = next;
         tail= next;
+        curr=next;
         i=1;
       }
     }
     if(newHead) return newHead;
     return head;
   }
+
+
+
+
+
+/**
+*  Recursive
+*/
+
+
+
+  // pair<ListNode*, ListNode*> reverseList(ListNode* head) {
+  //   ListNode* prev=NULL;
+  //   ListNode* newtail = head;
+  //   while(head){
+  //     ListNode* curr = head;
+  //     head= head->next;
+  //     curr->next = prev;
+  //     prev= curr;
+  //   }
+  //   return make_pair(prev, newtail );
+  // }
+
+  // void print(ListNode*head, ListNode*tail){
+  //   while(head!=tail && head!=NULL){
+  //     cout<<head->val<<" ";
+  //     head=head->next;
+  //   }
+  // }
+
+  // ListNode* reverseKGroup(ListNode* head, int k) {
+  //   if(head==NULL || head->next==NULL) return head;
+
+  //   ListNode* tail= head;
+  //   ListNode* newHead = head;
+
+
+  //   int i = 1;
+  //   while(tail!=NULL && tail->next!=NULL && i<k){
+  //     tail=tail->next;
+  //     i++;
+  //   }
+  
+  //   if(i<k) return head;
+  //   ListNode* rt = reverseKGroup(tail->next,k); //right part head
+  //   tail->next =NULL;
+  //   pair<ListNode*, ListNode*> reversed = reverseList(head);
+  //   newHead = reversed.first;
+  //   reversed.second->next = rt;
+  //   return newHead;
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
