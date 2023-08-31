4a38d044891a9d75f9167372df74a099c8c3a838
Ankush Rawat <ankushrawat950@gmail.com>
2023-08-31 19:27:52 +0530

    Time: 1155 ms (9.76%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,8 +1,16 @@
 # Write your MySQL query statement below
 
 
-select email from
-(select email, count(email) as count from Person group by email ) 
-as tmpTable  # a name is required for tmporary tables
 
-where count >1;
+## create a tmp table with count and use that count as a filter
+# select email from
+# (select email, count(email) as count from Person group by email ) 
+# as tmpTable  # a name is required for tmporary tables
+
+# where count >1;
+
+
+select email
+from Person
+group by email 
+having count(email)>1
