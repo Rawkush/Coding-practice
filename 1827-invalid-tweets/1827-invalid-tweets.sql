e64ae82ec6ce2bc37e0f500b3a15a9b2eab3b353
Ankush Rawat <ankushrawat950@gmail.com>
2023-09-01 21:55:01 +0530

    Time: 1674 ms (15.68%), Space: 0B (100%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.sql
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
 
 
-select tweet_id from 
-  (select tweet_id,
-        CHAR_LENGTH(content) as cl
-  from Tweets) as tmp
-where  cl > 15
+# select tweet_id from 
+#   (select tweet_id,
+#         CHAR_LENGTH(content) as cl
+#   from Tweets) as tmp
+# where  cl > 15
+
+
+select tweet_id  from Tweets where length(content) > 15
