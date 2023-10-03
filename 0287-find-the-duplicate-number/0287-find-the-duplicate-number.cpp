82f2df111e199ea1f6d8ed67dd27490f998c7e71
Ankush Rawat <ankushrawat950@gmail.com>
2023-10-03 23:38:51 +0530

    Time: 80 ms (72.11%), Space: 61.6 MB (48.3%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+ 
+ /**
+ * Two pointers
+ * TC O(n)
+ * SC O(1)
+ */
+ 
+  int findDuplicate(vector<int>& nums) {
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+    //since number must exist according to pigeon hole principle
+    // therefore this loop will terminate
+    while(slow!=fast){
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    }
 
+    // now i have two LL
+    // head1 => 0 , tail1 = slow
+    // head2 => nums[slow], tail2 = slow
+    int head1 = 0;
+    int head2 = fast;
+    while(head1 != head2){
+      head1 = nums[head1];
+      head2 = nums[head2];
+    }
+    return head1;
+  }
+  
 
   /**
     use array as hashmap
@@ -24,13 +52,13 @@ public:
   /**
   -ve marking
   */
-  int findDuplicate(vector<int>& nums) {
-    for(int i=0; i<nums.size(); i++){
-      int index = abs(nums[i]);
-      int itemAtIdx = nums[index];
-      if(itemAtIdx<0) return index;
-      nums[index] = -nums[index];
-    }
-    return 0;
-  }
+  // int findDuplicate(vector<int>& nums) {
+  //   for(int i=0; i<nums.size(); i++){
+  //     int index = abs(nums[i]);
+  //     int itemAtIdx = nums[index];
+  //     if(itemAtIdx<0) return index;
+  //     nums[index] = -nums[index];
+  //   }
+  //   return 0;
+  // } 
 };
