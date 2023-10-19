2527ef1da28aecad48c16c9fe732d15e96a46f0d
Ankush Rawat <ankushrawat950@gmail.com>
2023-10-19 23:07:59 +0530

    Time: 119 ms (58.33%), Space: 46.5 MB (44.79%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+/**
+All subarray sizes till size k - all subarray sizes till size k-1 =  all subarray of exact size k
 
+Well, suppose you have a room with a bunch of children in it. It turns out that there are 12 children who are five years old or under, and 8 children who are four years old or under.
+
+Now presumably you agree that each one of the "children four years old or under" is also one of the "children five years old or under," right?
+
+So suppose we take the 12 children five years old or younger, and remove from them the 8 children four years old or younger. Of course, 12 - 8 = 4 children remain.
+
+*/
   // calculate all subset till valid
   int helper(vector<int>& nums, int k){
     unordered_map<int, int> mp; // value , count
