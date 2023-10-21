a23e81bf42f17505176170dab524fad4a946d45f
Ankush Rawat <ankushrawat950@gmail.com>
2023-10-21 21:27:15 +0530

    Time: 235 ms (69.62%), Space: 115.5 MB (57.14%) - LeetHub

--- 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.cpp
+++ 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
+// using sliding window
+  // int minimumCardPickup(vector<int>& cards) {
+  //   int mn = INT_MAX;
+  //   int l=0,r=0;
+  //   unordered_map<int,int>mp;
+  //   while(r<cards.size()){
+  //     mp[cards[r]]++;
+  //     if(mp[cards[r]]>1){
+  //       // duplicate found
+  //       //remove all unnecessary elements
+  //       while(cards[l]!=cards[r] || mp[cards[r]]>2){
+  //         mp[cards[l]]--;
+  //         if(mp[cards[l]]<=0) mp.erase(cards[l]);
+  //         l++;
+  //       }
+  //       mn = min(mn, r-l+1);
+  //     }
+  //     r++;
+  //   }
+
+  //   return mn == INT_MAX ? -1 : mn;
+  // }
+
   int minimumCardPickup(vector<int>& cards) {
     int mn = INT_MAX;
     int l=0,r=0;
     unordered_map<int,int>mp;
-    while(r<cards.size()){
-      mp[cards[r]]++;
-      if(mp[cards[r]]>1){
-        // duplicate found
-        //remove all unnecessary elements
-        while(cards[l]!=cards[r] || mp[cards[r]]>2){
-          mp[cards[l]]--;
-          if(mp[cards[l]]<=0) mp.erase(cards[l]);
-          l++;
-        }
-        mn = min(mn, r-l+1);
+    for(int i=0; i<cards.size(); i++){
+      if(mp.count(cards[i])==0){
+        mp[cards[i]] = i;
+      }else{
+        int lastIdx = mp[cards[i]];
+        mn = min(mn, i-lastIdx +1);
+        mp[cards[i]] = i;
       }
-      r++;
     }
-
     return mn == INT_MAX ? -1 : mn;
-  }
+   }
 };
