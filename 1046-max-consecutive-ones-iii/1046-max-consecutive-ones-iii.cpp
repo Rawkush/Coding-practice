4573ee989a5e91a0db385e6f5247355df93698f8
Ankush Rawat <ankushrawat950@gmail.com>
2023-10-21 23:49:27 +0530

    Time: 39 ms (84.65%), Space: 55.9 MB (25.18%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -7,17 +7,24 @@ public:
     int mx=0;
     while(r<nums.size()){
      if(nums[r]==0){    
-        if(count>0){ // allow fliping of this 0
-          count--;
-        }
-        else{
-          while(nums[l]!=0){
-            l++;
-          }
+        // if(count>0){ // allow fliping of this 0
+        //   count--;
+        // }
+        // else{
+        //   // remove all elements till we find 0
+        //   while(nums[l]!=0){
+        //     l++;
+        //   }
+        //   // remove this first 0, so our current 0 element i.e nums[r] can be flipped
+        //   l++;
+        // }
+
+        count--;
+        while(count<0){
+          if(nums[l]==0)count++;
           l++;
         }
       }
-      // cout<<l<<" "<<r<<" "<<count<<endl;
       mx = max(r-l+1, mx);
       r++;
     }
