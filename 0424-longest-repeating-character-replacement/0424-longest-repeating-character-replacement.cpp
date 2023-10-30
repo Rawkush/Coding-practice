74b24272f88cc6bee31ba38949a653d95cfda795
Ankush Rawat <ankushrawat950@gmail.com>
2023-10-30 22:53:21 +0530

    Time: 10 ms (54.13%), Space: 7.3 MB (49.87%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
 
+/**
+The problem says that we can make at most k changes to the string (any character can be replaced with any other character). So, let's say there were no constraints like the k. Given a string convert it to a string with all same characters with minimal changes. The answer to this is
+
+length of the entire string - number of times of the maximum occurring character in the string
+
+Given this, we can apply the at most k changes constraint and maintain a sliding window such that
+
+(length of substring - number of times of the maximum occurring character in the substring) <= k
+*/
+
+
   int getMax(vector<int> &fcount){
     int mx = INT_MIN;
     for(auto x:fcount){
