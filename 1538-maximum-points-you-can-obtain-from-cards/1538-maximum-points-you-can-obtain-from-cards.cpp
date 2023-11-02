af532a29ee016caab54ef7f179e675cb00d1c2f0
Ankush Rawat <ankushrawat950@gmail.com>
2023-11-02 23:34:34 +0530

    Time: 56 ms (10.86%), Space: 42.6 MB (79.71%) - LeetHub

---
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+/**
+    * our sliding window size is k,
+    * since we can pick only from start or end, that means we will have continuous array, assuming if the start and end are connected, like there is cycle
+    * only first k and k elemenst  will be used at any case, we can never have a case where other item can come into play as we can pick only from start or end
+    *
+    */
+  int maxScore(vector<int>& cardPoints, int k) {
+    int sum=0;
+    for(int i=0; i<k; i++) sum +=cardPoints[i]; //sum if all elements are picked from start
+    
+    int res=sum;
+    // check if we move the window, i.e take one element from back will we get more score
+    for(int i=k-1, j=1; i>=0; i--, j++){
+      sum-=cardPoints[i];
+      sum+=cardPoints[cardPoints.size()-j];
+      res = max(res, sum);
+    }
+    return res;
+  }
+};
