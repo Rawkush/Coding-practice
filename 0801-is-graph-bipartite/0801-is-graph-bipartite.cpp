7bca48384aa23e8051bb0f111f32c3232ef23855
Ankush Rawat <ankushrawat950@gmail.com>
2024-09-11 00:02:01 +0530

    Time: 12 ms (90.31%), Space: 16.9 MB (21.49%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -4,35 +4,64 @@ public:
      * a graph is bipartite if we color nodes with 2 color, and each node colr
      * should not be equal to its neighbour
      */
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> vis(n, -1);
-        queue<int> q;
-        for (int i = 0; i < n; i++) {
-            int color = 0; // 0, 1, two colors
-            if (vis[i] == -1) {
-                q.push(i);
-                vis[i] = color;
-                color = 1;
-            }
-            while (!q.empty()) {
-                int lvl = q.size();
-                for (int j = 0; j < lvl; j++) {
-                    int cnode = q.front();
-                    q.pop();
-                    vector<int> adnodes = graph[cnode];
-                    for (auto x : adnodes) {
-                        if (vis[x] == -1) {
-                            q.push(x);
-                            vis[x] = color;
-                        } else if (vis[x] != color) {
-                            return false;
-                        }
-                    }
-                }
-                color = (color + 1) % 2;
 
-            }
+    /**
+        BFS
+    */
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //     int n = graph.size();
+    //     vector<int> vis(n, -1);
+    //     queue<int> q;
+    //     for (int i = 0; i < n; i++) {
+    //         int color = 0; // 0, 1, two colors
+    //         if (vis[i] == -1) {
+    //             q.push(i);
+    //             vis[i] = color;
+    //             color = 1;
+    //         }
+    //         while (!q.empty()) {
+    //             int lvl = q.size();
+    //             for (int j = 0; j < lvl; j++) {
+    //                 int cnode = q.front();
+    //                 q.pop();
+    //                 vector<int> adnodes = graph[cnode];
+    //                 for (auto x : adnodes) {
+    //                     if (vis[x] == -1) {
+    //                         q.push(x);
+    //                         vis[x] = color;
+    //                     } else if (vis[x] != color) {
+    //                         return false;
+    //                     }
+    //                 }
+    //             }
+    //             color = (color + 1) % 2;
+
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    /**
+    *     DFS
+    */
+ 
+    bool helper(vector<vector<int>>&graph, vector<int>&vis, int node, int color){
+        if(vis[node]!=-1) return vis[node]==color;
+        vis[node] = color;
+        vector<int> adnodes= graph[node];
+        for(auto x:adnodes){
+            if(!helper(graph, vis, x, !color)) return false;
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> vis(graph.size(), -1);
+        for(int i=0; i<graph.size(); i++){
+            if(vis[i]!=-1) continue;
+            bool res = helper(graph, vis, i, 0);
+            if(!res) return false;
         }
         return true;
     }
