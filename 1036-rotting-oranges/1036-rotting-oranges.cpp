eb5d9405772b1e351baf7484659e1b2efe4b58f5
Ankush Rawat <ankushrawat950@gmail.com>
2025-05-29 00:16:56 +0530

    Time: 0 ms (100%), Space: 16.6 MB (88.04%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>> q;
-        int m = grid.size();
-        int n = grid[0].size();
-        //push all rotten oranges to queue
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]==2) q.push({i , j});
-            }
-        }
-
-        int t=0;
-        // BFS
-        while(!q.empty()){
-            t++;
-            int s = q.size();
-            for(int k=0; k<s; k++){
-                pair<int, int> p = q.front();
-                int i = p.first;
-                int j = p.second;
-                q.pop();
-                //up
-                if(i>0 && grid[i-1][j]==1){
-                    grid[i-1][j]=2;
-                    q.push({ i-1, j});
-                }
-                // down
-                if(i<m-1 && grid[i+1][j]==1){
-                    grid[i+1][j] = 2;
-                    q.push({i+1, j});
-                }
-                //left
-                if(j>0 && grid[i][j-1]==1){
-                    grid[i][j-1] = 2;
-                    q.push({i, j-1});
-                }
-                //right
-                if(j<n-1 && grid[i][j+1]==1){
-                    grid[i][j+1] = 2;
-                    q.push({i, j+1});
-                }
-            }
-        }
-
-        // check if any fresh orange is remaining
-           for(int i=0; i<grid.size(); i++){
-            for(int j=0; j< grid[0].size(); j++){
-                if(grid[i][j]==1) return -1;
-            }
-        }
-
-        return t>0? t-1: t;     
+  int orangesRotting(vector<vector<int>>& grid) {
+    queue<pair<int,int>> q;
+    for(int i=0; i<grid.size(); i++){
+      for(int j=0; j<grid[0].size(); j++){
+        if(grid[i][j]==2) q.push({i,j});
+      }
     }
+
+    int count = 0;
+    while(!q.empty()){
+      count++;
+      int level = q.size();
+      for(int i=0; i<level; i++){
+        pair<int,int> p = q.front();
+        int x =p.first;
+        int y= p.second;
+        q.pop();
+        //left
+        if(y>0 && grid[x][y-1]==1){
+          grid[x][y-1] = 2;
+          q.push({x, y-1});
+        }
+        //right
+        if(y<grid[0].size()-1 && grid[x][y+1]==1){
+          grid[x][y+1] = 2;
+          q.push({x, y+1});
+        }
+        //down
+        if(x<grid.size()-1 && grid[x+1][y]==1){
+          grid[x+1][y] = 2;
+          q.push({x+1, y});
+        }
+        //up
+        if(x>0 && grid[x-1][y]==1){
+          grid[x-1][y] = 2;
+          q.push({x-1, y});
+        }
+      }
+    }
+    for(int i=0; i<grid.size(); i++){
+      for(int j=0; j<grid[0].size(); j++){
+        if(grid[i][j]==1) return -1;
+      }
+    }
+
+    return count>0?  count -1: count;
+  }
 };
