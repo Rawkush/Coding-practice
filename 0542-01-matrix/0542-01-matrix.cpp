0950c9bec5584afdcad3fab2a4acd90f23837799
Ankush Rawat <ankushrawat950@gmail.com>
2025-06-02 23:24:03 +0530

    Time: 19 ms (60.91%), Space: 36.7 MB (60.32%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,56 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int,int>> q;
-        int m = mat.size();
-        int n = mat[0].size();
+  vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+      queue<pair<int, int>> q;
+      int m = mat.size();
+      int n = mat[0].size();
+      vector<vector<bool>>isVisited(m, vector<bool>(n, false));
+      for (int i = 0; i < m; i++) {
+          for (int j = 0; j < n; j++) {
+              if (mat[i][j] == 0){
+                  q.push({i, j});
+                  isVisited[i][j]=true;    
+              }
+          }
+      }
 
-        vector<vector<bool>> isVisited(m, vector<bool>(n, false));
-        vector<vector<int>> res(m, vector<int>(n, 0));
+      vector<vector<int>>res(m, vector<int>(n, 0));
+      int d = 0;
+      while (!q.empty()) {
+          int s = q.size();
+          for (int i = 0; i < s; i++) {
+              pair<int, int> p = q.front();
+              q.pop();
+              int r = p.first;
+              int c = p.second;
+              res[r][c]=d;
+              if (r > 0 && !isVisited[r - 1][c]) {
+                  q.push({r - 1, c});
+                  isVisited[r - 1][c] = true;
+              }
+              // down1
+              if (r < m - 1 && !isVisited[r + 1][c]) {
+                  q.push({r + 1, c});
+                  isVisited[r + 1][c] = true;
+              }
+              // left
+              if (c > 0 && !isVisited[r][c - 1]) {
+                  q.push({r, c - 1});
+                  isVisited[r][c - 1] = true;
+              }
+              // down
+              if (c < n - 1 && !isVisited[r][c + 1]) {
+                  q.push({r, c + 1});
+                  isVisited[r][c + 1] = true;
+              }
+          }
+          d++;
+      }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(mat[i][j]==0){
-                    q.push({i, j});
-                    isVisited[i][j] =true;
-                }
-            }
-        }
-
-        int d=0;
-        while(!q.empty()){
-            int s = q.size(); // all items in same level
-            for(int i=0; i<s; i++){
-                pair<int,int> p = q.front();
-                q.pop();
-
-                int r = p.first;
-                int c = p.second;
-                res[r][c] = d;
-                // up
-                if(r>0 && !isVisited[r-1][c]){
-                    q.push({r-1, c});
-                    isVisited[r-1][c] = true;
-                }
-                //down1
-                if(r<m-1 && !isVisited[r+1][c]){
-                    q.push({r+1, c});
-                    isVisited[r+1][c] = true;
-                }
-                //left
-                if(c>0 && !isVisited[r][c-1]){
-                    q.push({r, c-1});
-                    isVisited[r][c-1] = true;
-                }
-                //down
-                if(c<n-1 && !isVisited[r][c+1]){
-                    q.push({r, c+1});
-                    isVisited[r][c+1] = true;
-                }
-            }
-            d++;
-        }
-
-        return res;
-    }
+      return res;
+  }
 };
