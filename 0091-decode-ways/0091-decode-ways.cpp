279231dfe5cf611557b38de178f9dc66d2ffac63
Ankush Rawat <ankushrawat950@gmail.com>
2026-02-10 01:16:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (81.84%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    int helper(string s, int idx, vector<int>&dp) {
-        if(idx>=s.size()) return 1;
-        if(s[idx] == '0' ) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        //if i use first character
-        int c = helper(s, idx+1, dp);
-        //if we use two characters together
-        int d = 0;
-        if(idx+1<s.size() && ((s[idx]-'0') * 10 + (s[idx+1]-'0')) <=26 ) {
-            d = helper(s, idx+2, dp);
-        }
-        return dp[idx]=c+d;
-    }
+    /** Iterative DP */
     int numDecodings(string s) {
-        vector<int> dp(s.size(), -1);
-        return helper(s, 0, dp);
+        vector<int> dp(s.size()+1, 0);
+        dp[0] = 0;
+        if(s[0]=='0') dp[0] = 0;
+        else dp[0] = 1;
+        for(int i=1; i<=s.size(); i++) {
+            if(s[i-1] != '0') {
+                dp[i] = dp[i-1];
+            }
+            // check if this is two
+            if(i==1) continue;
+            int twoDigit = (s[i-2] - '0') * 10 + (s[i-1] - '0');
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[s.size()];
     }
+
+
+
+    // int helper(string s, int idx, vector<int>&dp) {
+    //     if(idx>=s.size()) return 1;
+    //     if(s[idx] == '0' ) return 0;
+    //     if(dp[idx]!=-1) return dp[idx];
+    //     //if i use first character
+    //     int c = helper(s, idx+1, dp);
+    //     //if we use two characters together
+    //     int d = 0;
+    //     if(idx+1<s.size() && ((s[idx]-'0') * 10 + (s[idx+1]-'0')) <=26 ) {
+    //         d = helper(s, idx+2, dp);
+    //     }
+    //     return dp[idx]=c+d;
+    // }
+    // int numDecodings(string s) {
+    //     vector<int> dp(s.size(), -1);
+    //     return helper(s, 0, dp);
+    // }
 };
