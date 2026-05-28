2429a6dde1b0a4764f35eec4f963aa4e06bbfc42
Ankush Rawat <ankushrawat950@gmail.com>
2026-05-28 22:03:07 +0530

    Time: 699 ms (56.79%), Space: 782 MB (5.25%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -1,64 +1,61 @@
-const int MAXNODES = 5e6;
-
 class Node {
-public:
-    array<Node*, 26> nextNode;
-    int idx;
-    int size;
-    Node(int i, int s) : idx(i), size(s) {
-        nextNode.fill(nullptr);
-    }
-    Node() {}
+    public:
+        vector<int> nextNode;  // remove (26) from here
+        int idx;
+        int size;
+        Node(int i, int s) : nextNode(26, -1) {  // initialize here
+            idx = i;
+            size = s;
+        }
 };
-
-Node pool[MAXNODES];
-int poolIdx = 0;
-
-Node* newNode(int i, int s) {
-    pool[poolIdx].idx = i;
-    pool[poolIdx].size = s;
-    pool[poolIdx].nextNode.fill(nullptr);
-    return &pool[poolIdx++];
-}
 
 class Solution {
 public:
+
+    vector<Node*> trieNodes;
+
     void addInTrie(string &s, int idx, Node *root) {
         int size = s.size();
         for(int i=size-1; i>=0; i--) {
-            if(root->nextNode[s[i]-'a'] == NULL) {
-                Node *node = newNode(idx, size);
-                root->nextNode[s[i]-'a'] = node;
-                root = node;
+            /** if there is no node create new insert and move */
+            if(root->nextNode[s[i]-'a']==-1) {
+                Node *node = new Node(idx, size);
+                trieNodes.push_back(node);
+                int trieNodeIdx = trieNodes.size()-1;
+                root->nextNode[s[i]-'a'] = trieNodeIdx;
+                root=node;
             } else {
-                root = root->nextNode[s[i]-'a'];
-                if(root->size > size) {
+                /** if node exists check if current string size if small or not */
+                root = trieNodes[root->nextNode[s[i]-'a']];
+                if(root->size > size ) {
                     root->size = size;
                     root->idx = idx;
                 }
-            }
+            }         
         }
     }
 
     int existsInTrie(Node *root, string &s) {
-        int idx = -1;
+        int idx =-1;
         for(int i=s.size()-1; i>=0; i--) {
-            if(root->nextNode[s[i]-'a'] == NULL) return idx;
-            root = root->nextNode[s[i]-'a'];
+            if(root->nextNode[s[i]-'a'] == -1) return idx;
+            root = trieNodes[root->nextNode[s[i]-'a']];
             idx = root->idx;
         }
         return root->idx;
     }
 
+
+
     vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
-        poolIdx = 0;  // reset pool for each test case
-        Node *root = newNode(0, INT_MAX);
+        Node *root = new Node(0, INT_MAX);
+        trieNodes.push_back(root);
 
         int smallestStringIdx = 0;
         int smallestStringSize = INT_MAX;
         for(int i=0; i<wordsContainer.size(); i++) {
             addInTrie(wordsContainer[i], i, root);
-            if(smallestStringSize > (int)wordsContainer[i].size()) {
+            if(smallestStringSize > wordsContainer[i].size()) {
                 smallestStringIdx = i;
                 smallestStringSize = wordsContainer[i].size();
             }
@@ -66,10 +63,17 @@ public:
 
         vector<int> res(wordsQuery.size(), smallestStringIdx);
         for(int i=0; i<wordsQuery.size(); i++) {
+            // string s ="";
+            // for(int j=wordsQuery[i].size()-1; j>=0; j--) {
+                // s+=wordsQuery[i][j];
             int idx = existsInTrie(root, wordsQuery[i]);
-            if(idx == -1) continue;
+            if(idx==-1) {
+                continue;
+            }
             res[i] = idx;
+            // }
         }
+        // printMap(mp);
         return res;
     }
 };
