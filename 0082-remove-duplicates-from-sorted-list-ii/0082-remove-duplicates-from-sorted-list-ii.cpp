3fd22362d90ebd235155643be1834b11514cd247
Ankush Rawat <ankushrawat950@gmail.com>
2026-06-11 22:59:34 +0530

    Time: 2 ms (10.1%), Space: 15.8 MB (37.03%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,11 +10,7 @@
  */
 class Solution {
 public:
-
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(!head || head->next == NULL) return head; //NULL or one item
-
-        //if starting items are deplicate keep rmoveing them
+    ListNode* remove(ListNode* head) {
         ListNode *tmpHead = head->next;
         //Delete head
         while(tmpHead && tmpHead->val == head->val) {
@@ -27,19 +23,34 @@ public:
                 tmpHead = tmpHead->next;
             }
         }
-        // after this all duplicate items from head should be removed
+        return head;
+    }
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || head->next == NULL) return head; //NULL or one item
+
+        //if starting items are deplicate keep rmoveing them
+        // ListNode *tmpHead = head->next;
+        // //Delete head
+        // while(tmpHead && tmpHead->val == head->val) {
+        //     tmpHead = tmpHead->next;
+        //     if(tmpHead == NULL) return NULL; // all same items
+
+        //     if(tmpHead->val != head->val) {
+        //         //now we have a node this we need so 
+        //         head = tmpHead;
+        //         tmpHead = tmpHead->next;
+        //     }
+        // }
+        // // after this all duplicate items from head should be removed
+        head = remove(head); // remove duplicates from start
+        if(!head) return head;
         ListNode *prev = head;
         ListNode *curr = head->next;
         while(curr) {
-            bool isDuplicate = false;
-            while(curr->next && curr->next->val == curr->val) {
-                curr=curr->next;
-                isDuplicate =true;
-            }
-            if(isDuplicate) {
-                prev->next = curr->next;
-            } else prev = prev->next;
-            curr = curr->next;
+            curr = remove(curr);
+            prev->next = curr;
+            if(curr) curr=curr->next;
+            prev= prev->next;
         }
         // prev->next = curr;
         return head;
