379ce8be037b47076564b17a96227aa59f8dfa59
Ankush Rawat <ankushrawat950@gmail.com>
2026-06-17 18:38:52 +0530

    Time: 0 ms (100%), Space: 39.8 MB (69.58%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res = "";
+        for(char c: s) {
+            if(c=='*') {
+                if(res.size()>0) res.pop_back();
+            }
+            else if(c=='#') {
+                res += res;
+            }
+            else if(c=='%') {
+                reverse(res.begin(), res.end());
+            }
+            else {
+                res+=c;
+            }
+            // cout<<res<<endl;
+        }
+        return res;
+    }
+};
