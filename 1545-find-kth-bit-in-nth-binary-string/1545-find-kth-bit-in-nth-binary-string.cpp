dd3be843c99d07d49cd22c1f1250a130de2ff4f5
Ankush Rawat <ankushrawat950@gmail.com>
2026-06-19 13:18:07 +0530

    Time: 72 ms (15.22%), Space: 73.5 MB (17.67%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-
-    char helper(int si, int k, int slen) {
-        if(si==1) return '0';
-        int mid = (slen/2) + 1;
-        if(mid == k) return '1';
-        if(k>mid) {
-            //flip if on right side
-            return helper(si-1, slen-k + 1, mid-1)=='1'? '0' : '1';
+    // string reverse(const string &s) {
+    //     string ss = "";
+    //     for (auto x : s) {
+    //         ss = x + ss;
+    //     }
+    //     return ss;
+    // }
+    string invert(string& s) {
+        string ss = "";
+        for (auto x : s) {
+            if (x == '1')
+                ss += '0';
+            else
+                ss += '1';
         }
-        else {
-            return helper(si-1, k, mid-1);
-        }
+        return ss;
     }
+    string helper(int n, int k, string &s) {
+        if (n <= 0 || s.size() >= k)
+            return s;
+        string tmp = s;
+        reverse(tmp.begin(), tmp.end() );
 
+        s = s + "1" + invert(tmp);
+        return helper(n-1, k, s);
+        // return ss;
+    }
     char findKthBit(int n, int k) {
-        /**
-        *   if you see s0 =. 0
-        * s1 = s0 + 1 + reverse(inert(s0));
-        * there len(si) = len(si-1) + 1 + len(si-1);
-        * => 2*len(si-1) + 1 
-        */
-        int slen=0;
-        int i=1;
-        for( ; i<=n; i++) {
-            slen = 2*slen + 1;
-            if(slen>=k) break;
-        }
-
-        return helper(i, k, slen);
+        string s ="0";
+        s = helper(n, k, s);
+        
+        return s[k - 1];
     }
 };
