bb8dd4735f45c9f33d091cf343987332a2d0d76b
Ankush Rawat <ankushrawat950@gmail.com>
2026-07-02 15:33:23 +0530

    Time: 215 ms (35.79%), Space: 38.5 MB (65.99%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,69 +1,48 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-        return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' ||
+               ch == 'o' || ch == 'u';
     }
-    void print(unordered_map<char,int> &mp) {
-        for(auto x: mp) {
-            cout<<x.first<< " "<<x.second<<endl;
-        }
-    }
-    long long countOfSubstrings(string word, int k) {
-        unordered_map<char,int> mp;
-        long long l=0, r=0;
-        long long total =0;
-        long long consonantCount = 0;
-        long long prefixCount = 1;
-        while(r<word.size()) {
-            if(isVowel(word[r])) mp[word[r]]++;
-            else consonantCount++;    
-            // cout<<l<<" "<<r<<" "<<consonantCount<<" "<<total<<endl;
-        
-            // shrink the window
-            if(consonantCount>k){
-                //shrink the windowuntil 
-                while(l<r && consonantCount >k ) {
-                    if(isVowel(word[l])) {
-                        mp[word[l]]--;
-                        if(mp[word[l]]==0) mp.erase(word[l]);
-                        // else if(mp.size()==5) {
-                        //     total +=prefixCount;
-                        // }
-                        l++;
-                    } else {
-                        consonantCount--;
-                        l++; 
-                        break;
-                    }
+
+    long long atLeastK(string &word, int k) {
+        int n = word.size();
+
+        unordered_map<char, int> mp;
+        int consonants = 0;
+        int left = 0;
+
+        long long ans = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            if (isVowel(word[right]))
+                mp[word[right]]++;
+            else
+                consonants++;
+
+            while (mp.size() == 5 && consonants >= k) {
+
+                // Every substring starting before 'left'
+                // and ending at 'right' is also valid.
+                ans += (n - right);
+
+                if (isVowel(word[left])) {
+                    mp[word[left]]--;
+                    if (mp[word[left]] == 0)
+                        mp.erase(word[left]);
+                } else {
+                    consonants--;
                 }
-                // if(consonantCount == k && mp.size()==5) total+=prefixCount;
-                prefixCount =1;
+
+                left++;
             }
-            while(consonantCount == k && mp.size()==5) {
-                if(!isVowel(word[l]) || mp[word[l]]<=1) break;
-                mp[word[l]]--;
-                prefixCount++;
-                l++;
-            } 
-            // cout<<"DAS "<<l<<" "<<r<<" "<<consonantCount<<" "<<total<<endl;
-            if(consonantCount == k && mp.size()==5) {
-                // cout<<"asdc "<<prefixCount<<endl;
-                total += prefixCount;
-            } 
-            r++;
         }
-        // print(mp);
-        // cout<<l<<" "<<r<<" "<<consonantCount<<" "<<total;
-        while(consonantCount >= k && l<r && mp.size()==5) {
-            if(isVowel(word[l])) {
-                mp[word[l]]--;
-                if(mp[word[l]]==0) break;
-            } else {
-                consonantCount--;
-            }
-            if(consonantCount == k && mp.size()==5) total++;
-            l++;
-        }
-        return total;
+
+        return ans;
+    }
+
+    long long countOfSubstrings(string word, int k) {
+        return atLeastK(word, k) - atLeastK(word, k + 1);
     }
 };
