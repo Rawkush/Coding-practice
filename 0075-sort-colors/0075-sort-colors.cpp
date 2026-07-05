c38e6b119aef6033ff22ba3ef7778faecf9cfae8
Ankush Rawat <ankushrawat950@gmail.com>
2026-07-05 18:08:29 +0530

    Time: 461 ms (0.65%), Space: 11.7 MB (11.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-  void sortColors(vector<int>& nums) {
-    int c1 = 0, c2=0, c3=0;
-    for(int i=0; i< nums.size(); i++){
-      if(nums[i]==0) c1++;
-      if(nums[i]==1) c2++;
-      if(nums[i]==2) c3++;
+    void print(vector<int>& nums) {
+        cout<<"nums; ";
+        for(auto x: nums) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
     }
-
-    int idx=0;
-    for(int i=0; i< c1; i++){
-      nums[idx++]=0;
+    void sortColors(vector<int>& nums) {
+        int zeroIdx =0, oneIdx =0, twoIdx = nums.size()-1; 
+        oneIdx = zeroIdx;
+        while(oneIdx <= twoIdx && oneIdx<nums.size() && twoIdx>=0 && zeroIdx<nums.size() ) {
+            // if(zeroIdx > oneIdx) oneIdx = zeroIdx;
+            if(nums[oneIdx] == 0) {
+                swap(nums[zeroIdx], nums[oneIdx]);
+                zeroIdx++;
+                oneIdx++;
+            } else if(nums[oneIdx]==2) {
+                swap(nums[oneIdx], nums[twoIdx]);
+                twoIdx--;
+            } else {
+                oneIdx++;
+            }
+            cout<< zeroIdx<<" " <<oneIdx<< " "<<twoIdx<< " "<<endl;
+            print(nums);
+        }
+        return;
     }
-    for(int i=0; i<c2; i++){
-      nums[idx++] =1;
-    }
-    for(int i=0; i< c3; i++){
-      nums[idx++]=2;
-    }
-  }
 };
