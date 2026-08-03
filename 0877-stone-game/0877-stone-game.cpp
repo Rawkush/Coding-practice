4e857c55fe27af3d23d3c2fe9c86f14dc9223b37
Ankush Rawat <ankushrawat950@gmail.com>
2026-08-03 13:41:20 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.89%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // bool helper(vector<int>& piles, int i, int j, int sum, int turn) {
+    //     if(i>j) return false;
+    //     if(sum<=0) return true;
+    //     int mx =0;
+    //     if()
+
+    // }
+
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
