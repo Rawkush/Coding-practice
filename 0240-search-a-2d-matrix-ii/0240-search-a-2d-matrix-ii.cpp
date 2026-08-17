603e9d3148a63c8939b16b70ffbd2ac781048518
Ankush Rawat <ankushrawat950@gmail.com>
2026-08-18 00:22:20 +0530

    Time: 36 ms (97.88%), Space: 18.7 MB (39.6%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    int findCol(vector<vector<int>>& matrix, int target, int l, int r) {
-        if(l>r) return l;
-        int mid = (l+r)/2;
-        if(matrix[0][mid]==target) return mid;
-        if(matrix[0][mid] > target) {
-            return findCol(matrix, target, l, mid-1);
-        }
-        if(l==r) return l;
-        return findCol(matrix, target, mid+1, r);
-    }
-
-    bool findRow(vector<vector<int>>& matrix, int target, int l, int r, int c) {
-        if(l>r) return false;
-        int mid = (l+r) / 2;
-        if(matrix[mid][c] == target) return true;
-        if(matrix[mid][c] > target) {
-            return findRow(matrix, target, l, mid - 1, c);
-        } else {
-            return findRow(matrix, target, mid + 1, r, c);
-        }
-    }
   
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int col = findCol(matrix, target, 0, matrix[0].size()-1);
-        while (col >= 0) {
-            //worst case N*logn
-            if (findRow(matrix, target, 0, matrix.size() - 1, col)) {
-                return true;
-            }
-            col--; 
+        int n = matrix.size()-1;
+        int m = matrix[0].size()-1;
+        int j=0;
+        int i=n;
+        while(i>=0 && j<=m) {
+            if(matrix[i][j]==target) return true;
+            if(target > matrix[i][j]) {
+               j++;
+            } else {
+                i--;
+            }            
         }
         return false;
     }
